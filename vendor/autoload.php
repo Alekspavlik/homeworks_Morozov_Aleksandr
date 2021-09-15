20c97fbb93077231b1d7401333fa2cff141d600c
oleksandrmorozov <morozovoleksandr86@gmail.com>
2021-09-15 16:30:46 +0300

    Сhanged autoload.php

---
+++ vendor/autoload.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vendor;
+
+class Autoloader
+{
+    protected $map;
+
+    public function addNamespace(string $prefix, string $dir)
+    {
+        $this->map[$prefix] = $dir;
+    }
+
+    public function register()
+    {
+        spl_autoload_register(array($this, "autoload"));
+    }
+
+    public function autoload($class)
+    {
+        $file = null;
+        foreach ($this->map as $prefix => $dir) {
+            if (strpos($class, $prefix) !== false) {
+                $class = explode("\\", $class);
+                array_shift($class);
+                $file = realpath($dir . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $class) . ".php");
+            }
+        }
+        if (null !== $file) {
+            include $file;
+        }
+    }
+}
+
+$autoload = new Autoloader();
+$autoload->addNamespace("App", "../src");
+$autoload->register();
